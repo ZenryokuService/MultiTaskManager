e8a1ce6797bab04d017b655f78f352f07b7d0e29
takk <yogotak45@gmail.com>
2018-10-13 12:05:53 +0900

    Hello LWJGLの起動完了：Success to execute "Hello LWJGL in Eclipse"

---
+++ gui.mtm/src/main/java/zenryokuservice/gui/jogl/prac/LwjglPracLv1.java
@@ -0,0 +1,118 @@
+package zenryokuservice.gui.jogl.prac;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * LWJGL初めの練習用クラスです。
+ * This Class is Hello LWJGL
+ * 
+ * @see https://github.com/SilverTiger/lwjgl3-tutorial/blob/master/src/silvertiger/tutorial/lwjgl/Introduction.java
+ * 
+ * @author takunoji
+ */
+public class LwjglPracLv1 {
+	/** ウィンドウハンドラ */
+	long window;
+
+	/**
+	 * 初めに動くメソッドです。全てはここから始まる。。。<br/>
+	 * Java program start from main method...<br/>
+	 * <ul>
+	 * <li>LWJGLの起動には"-XstartOnFirstThread"のVM引数がいるようだ</li>
+	 * <li>Javaのバージョンは1.8以降を使用する</li>
+	 * </ul>
+	 * @param args Array of String
+	 */
+	public static void main(String[] args) {
+		LwjglPracLv1 lv1 = new LwjglPracLv1();
+		// 2018/10/12 このメソッドを起動していないから動かなかった。。。
+		lv1.run();
+	}
+
+	public void run() {
+		System.out.println("Hello LWJGL " + Version.getVersion());
+		// 初期処理
+		init();
+		// ゲームループ
+		gameLoop();
+		
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+System.out.println("終了処理");
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();		
+	}
+
+	/**
+	 *  初期処理
+	 */
+	private void init() {
+		// エラー時のコールバックに標準エラー出力を設定する
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		// GLFWの初期化
+		if (!glfwInit()) {
+			throw new IllegalStateException("Unalbe to Initialize GLFW");
+		}
+		
+		// GLFWの設定処理
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		
+		window = glfwCreateWindow(300, 300, "Hello LWJGL", NULL, NULL);
+		
+		if(window == NULL) {
+			throw new RuntimeException("Failed to create GLFW Window");
+		}
+		
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+			});
+		// 2018/10/12 実装漏れ
+//		try (MemoryStack stack = stackPush()) {
+//			IntBuffer pWidth = stack.mallocInt(1);
+//			IntBuffer pHeight = stack.mallocInt(1);
+//			// ウィンドウサイズ指定
+//			glfwGetWindowSize(window, pWidth, pHeight);
+//			// 
+//			GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+//			// 画面の中央座標を設定する
+//			glfwSetWindowPos(
+//					window
+//					, (vidMode.width() - pWidth.get(0) / 2)
+//					, (vidMode.height() - pHeight.get(0)) / 2
+//			);
+//			// OpenGLコンテキストを作成する
+//			glfwMakeContextCurrent(window);
+//			// enable v-sync
+//			glfwSwapInterval(1);
+//			// 表示処理
+//			glfwShowWindow(window);
+//		}
+	}
+	// 写経元のコードでは「loop()」になっている
+	private void gameLoop() {
+		GL.createCapabilities();
+		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+		while ( !glfwWindowShouldClose(window) ) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+			glfwSwapBuffers(window); // swap the color buffers
+
+			glfwPollEvents();
+		}
+	}
+}
---
+++ gui.mtm/src/main/java/zenryokuservice/gui/lwjgl/tutoriral/DisplayManager.java
@@ -0,0 +1,14 @@
+package zenryokuservice.gui.lwjgl.tutoriral;
+
+import org.lwjgl.glfw.GLFW.Functions;
+
+/**
+ * 表示管理クラス
+ * 
+ * @author takunoji
+ */
+public class DisplayManager {
+	public static void createDisplay() {
+		
+	}
+}
